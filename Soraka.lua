4194d9cdfc53afd6728628812f01ef75cdc9dbe6
allwillburn <livefortoday470@gmail.com>
2017-07-30 11:13:35 -0400

    Create Soraka.lua

---
+++ Soraka.lua
@@ -0,0 +1,21 @@
+if GetObjectName(GetMyHero()) ~= "Soraka" then return end
+
+
+local SorakaMenu = Menu("Soraka", "Soraka")
+
+SorakaMenu:SubMenu("AutoMode", "AutoMode")
+SorakaMenu.AutoMode:Boolean("W", "Auto W", true)
+SorakaMenu.AutoMode:Slider("MHP", "My HP to use W", 300,0,1000,50)
+SorakaMenu.AutoMode:Slider("AHP", "Ally HP to use W", 300,0,1000,50)
+
+OnTick(function (myHero)
+ 
+
+
+--Auto W on ally
+   
+        if SorakaMenu.AutoMode.W:Value() and ValidTarget(ally, 2000) and GetCurrentHP(myHero) >= SorakaMenu.AutoMode.MHP:Value() then and GetCurrentHP(ally) <= SorakaMenu.AutoMode.AHP:Value() then
+            CastTargetSpell(ally, _W)
+        end
+    end
+end)
