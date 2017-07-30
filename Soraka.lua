c5e33fc885672a05d6c04763219c03cdd4bf0cfc
allwillburn <livefortoday470@gmail.com>
2017-07-30 14:19:30 -0400

    Update Soraka.lua

--- Soraka.lua
+++ Soraka.lua
@@ -11,12 +11,14 @@ SorakaMenu.AutoMode:Slider("MHP", "My HP to use W", 300,0,1000,50)
 SorakaMenu.AutoMode:Slider("AHP", "Ally HP to use W", 300,0,1000,50)
 
 OnTick(function (myHero)
+  
+  local target = GetCurrentTarget()
  
 
 
 --Auto W on ally
    
-        if SorakaMenu.AutoMode.W:Value() and ValidTarget(ally, 2000) and GetCurrentHP(myHero) >= SorakaMenu.AutoMode.MHP:Value() and GetCurrentHP(ally) <= SorakaMenu.AutoMode.AHP:Value() then
+        if SorakaMenu.AutoMode.W:Value() and ValidTarget(target, 2000) and GetCurrentHP(myHero) >= SorakaMenu.AutoMode.MHP:Value() and GetCurrentHP(ally) <= SorakaMenu.AutoMode.AHP:Value() then
             CastTargetSpell(ally, _W)
         end
     
